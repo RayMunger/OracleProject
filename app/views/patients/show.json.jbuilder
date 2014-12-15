37c2e4349604c94457681cd61941922ce340d7bf
RayMunger <Raymond.Munger@its.ny.gov>
2014-12-15 14:36:40 -0500

    Initial repository create

---
+++ app/views/patients/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @patient, :id, :firstname, :lastname, :email, :todaydate, :created_at, :updated_at
---
+++ db/migrate/20141215161502_create_patients.rb
@@ -0,0 +1,12 @@
+class CreatePatients < ActiveRecord::Migration
+  def change
+    create_table :patients do |t|
+      t.string :firstname
+      t.string :lastname
+      t.string :email
+      t.date :todaydate
+
+      t.timestamps
+    end
+  end
+end
